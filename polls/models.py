0bc580d04960b5ac25e05f28a90c3ce32f188dbb
sawady1203 <sawady.dev@gmail.com>
2025-07-23 16:32:37 +0900

    質問モデルの名前を単数形に変更し、関連する外部キーを更新

--- polls/models.py
+++ polls/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class Questions(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField(verbose_name="date published")
 
 
 class Choise(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choise_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
