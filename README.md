dcacfd690c8eafa016768d5f1a1000d8321363ac
sawady1203 <sawady.dev@gmail.com>
2025-07-23 13:21:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# これは何
+Django Tutorial をやっていくリポジトリです。
+
+```
+$ uv init .
+$ uv pip install -U pip
+$ uv add django
+```
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from django-tutorial-20250723!")
+
+
+if __name__ == "__main__":
+    main()
