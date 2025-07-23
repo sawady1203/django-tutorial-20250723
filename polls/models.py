fe61f8ae14ee24df842a8819e5b49e6856ea3908
sawady1203 <sawady.dev@gmail.com>
2025-07-23 16:29:01 +0900

    pollsアプリのモデル、URL、ビューを追加

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Questions(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(verbose_name="date published")
+
+
+class Choise(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choise_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
